af7b195925564c86eb3c604823900207864d8dc5
Hyunseok Hwang <jacogoby@gmail.com>
2023-03-12 21:39:51 -0700

    movement button!

--- screens/MovementOverview.js
+++ screens/MovementOverview.js
@@ -8,6 +8,7 @@ import React, { useContext } from 'react';
 import Themes from '../assets/Themes';
 import { MaterialIcons } from '@expo/vector-icons'; 
 import Movement from '../components/Movement';
+import ReflectedMotions from '../components/ReflectedMotions';
 import Emotion from '../components/Emotion';
 import EmotionTimeline from '../components/EmotionTimeline';
 import Timeline from 'react-native-timeline-flatlist';
@@ -31,12 +32,13 @@ export default function MovementOverview({ route }) {
         circleColor: '#aaa',
         lineColor: '#aaa',
         circleSize: 10,
+        
         detailContainerStyle: {
           borderRadius: 10,
           borderWidth: 1,
           borderColor: 'black',
           padding: 10,
-          margin: 10
+          margin: 10,
         }
   
       };
@@ -78,7 +80,9 @@ export default function MovementOverview({ route }) {
                         <Text style={styles.text}>On {date}, you were feeling </Text>
                         <Text style={styles.text}>{uniqueFeelings.join(', ')}</Text>
                     </View>
+                    <View style= {{height:'100%',marginRight: 40}}>
                 <Timeline {...timelineProps} data={data}/> 
+                </View>
                     <View style={styles.backArrowBox}>
                         <MaterialIcons name="keyboard-backspace" size={50} color="black" onPress={() => navigator.navigate('CalendarScreen')}/>
                     </View>
@@ -102,10 +106,10 @@ const styles = StyleSheet.create({
     },
     motionsList: {
         height: '41%',
-        width: '100%',
+        width: '90%',
     },
     movementBox: {
-        marginLeft: '28.5%',
+        marginLeft: '34%',
         marginTop: 30,
         alignItems: 'center',
         justifyContent: 'center',
@@ -118,7 +122,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'center',
-        margin: 10
+        margin: 10,
+        marginBottom: 30,
     },
     backArrowBox: {
         height: 50,
