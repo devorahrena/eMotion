4bd40069d31ae4db8e3f9d14bd17d6d9f99eb73b
devorahrena <devorahrsimon@gmail.com>
2023-03-14 01:40:49 -0700

    similarEmotions now also associates movements
    this implementation will also win no awards but i would rather have this than restructure our database :)

--- utils/similarEmotions.js
+++ utils/similarEmotions.js
@@ -9,21 +9,39 @@ onValue(movementDataRef, (snapshot) => {
   for (let i = 0; i < data.length; i++) {
     if(data[i].motionEntry) { //loop through every place where feelings are recorded
       for (let j = 0; j < data[i].motionEntry.length; j++) {//there can be multiple motion entries  
-        if (data[i].motionEntry[j].feelings) {
+        if (data[i].motionEntry[j].feelings && (data[i].motionEntry[j].name !== "")) {
           for (let k = 0; k < data[i].motionEntry[j].feelings.length; k++) {//loop through every feeling and make sure it's in similarEmotions
             let primaryEmotion = data[i].motionEntry[j].feelings[k];
             if(!similarEmotions[primaryEmotion]) {//if there is not already an entry in similarEmotions (the big one) for the primaryEmotion in question
-              let associatedEmotions = [];
-              for (let m = 0; m < data[i].motionEntry[j].feelings.length; m++) {
+              for (let m = 0; m < data[i].motionEntry[j].feelings.length; m++) {//for each of the associated feelings, create an object
                 if (data[i].motionEntry[j].feelings[m] !== primaryEmotion) {
-                  associatedEmotions.push(data[i].motionEntry[j].feelings[m]);
+                  let obj = {}
+                  let associatedMovements = [];
+                  associatedMovements.push(data[i].motionEntry[j].name);
+                  obj[data[i].motionEntry[j].feelings[m]] = associatedMovements;
+                  similarEmotions[primaryEmotion] = obj;
                 }
               }
-              similarEmotions[primaryEmotion] = associatedEmotions;
+            // }
             } else { //if similarEmotions already has an entry for this primaryEmotion
-              for (let m = 0; m < data[i].motionEntry[j].feelings.length; m++) {
-                if ((data[i].motionEntry[j].feelings[m] !== primaryEmotion) && (!similarEmotions[primaryEmotion].includes(data[i].motionEntry[j].feelings[m]))) {
-                    similarEmotions[primaryEmotion].push(data[i].motionEntry[j].feelings[m]);
+              for (let m = 0; m < data[i].motionEntry[j].feelings.length; m++) {//for each of the associated feelings, update the object
+                if(data[i].motionEntry[j].feelings[m] !== primaryEmotion) {
+                  let obj = similarEmotions[primaryEmotion];
+                  if(obj[data[i].motionEntry[j].feelings[m]]) { //if this secondary feeling has already been associated with the primary one
+                    let associatedMovements = obj[data[i].motionEntry[j].feelings[m]];
+                    if (!associatedMovements.includes(data[i].motionEntry[j].name)) { //as long as this movement name is not already associated
+                      similarEmotions[primaryEmotion][data[i].motionEntry[j].feelings[m]].push(data[i].motionEntry[j].name);
+                      // console.log("movement name is not already associated");
+                    }
+                  } else { //if this secondary feeling has NOT been associated with the primary one
+                    //ie, secondary emotion is "joyful" and primary is "peaceful" and the object looks like:
+                    //peaceful: {"powerful": ['went on walk', 'other activity']}
+                    // console.log("secondary feeling has NOT been associated with the primary one");
+                    let associatedMovements = [];
+                    associatedMovements.push(data[i].motionEntry[j].name);
+                    obj[data[i].motionEntry[j].feelings[m]] = associatedMovements;
+                    similarEmotions[primaryEmotion] = obj;
+                  }
                 }
               }
             }
@@ -34,5 +52,10 @@ onValue(movementDataRef, (snapshot) => {
   }
   console.log("similar emotions: " + JSON.stringify(similarEmotions));
 });
+
+
+//{peaceful: {"powerful": ['went on walk', 'other activity'], "joyful": ["meditated"]}}
+//similarEmotions[primaryEmotion] returns an object
+//similarEmotions[primaryEmotion][secondaryEmotion] returns an array of movements
 
 export default similarEmotions;
