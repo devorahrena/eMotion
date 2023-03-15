ac7fc309f09796a5faac3760bcedf806f7decc0d
Sean Lee <slee1040@stanford.edu>
2023-03-15 01:33:35 -0700

    Update DuringMotion.js

--- screens/DuringMotion.js
+++ screens/DuringMotion.js
@@ -166,6 +166,7 @@ const styles = StyleSheet.create({
         top: 60
     },
     startButton: {
+      position: 'absolute',
       alignSelf: 'center',
       right: 0,
       backgroundColor: '#aaa',
